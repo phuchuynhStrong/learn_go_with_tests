ca710f213979ecea7e75cac8b4fcf5c5c7a0a564
Phuc Huynh <phuchuynh.strong@gmail.com>
2021-07-09 22:32:44 +0700

    Very first step of TDD with Go

---
+++ hello.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const frenchHelloPrefix = "Bonjour, "
+const spanishHelloPrefix = "Hola, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
+
+}
+
+func main() {
+	fmt.Println(Hello("Chris", ""))
+}
