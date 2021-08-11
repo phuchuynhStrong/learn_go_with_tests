69857b734c3332f53eff8baa891dce0cc5144fce
Phuc Huynh <phuchuynh.strong@gmail.com>
2021-08-12 00:57:40 +0700

    Select

--- select/select_test.go
+++ select/select_test.go
@@ -1,20 +1,23 @@
 package select_test
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
 )
 
-func Racer(a, b string) string {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration > bDuration {
-		return b
+func Racer(a, b string) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
-	return a
+
 }
 
 func measureResponseTime(url string) time.Duration {
@@ -31,24 +34,48 @@ func makeDelayServer(d time.Duration) *httptest.Server {
 }
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayServer(20 * time.Millisecond)
-	fastServer := makeDelayServer(0 * time.Millisecond)
+	t.Run("Test with delayed server", func(t *testing.T) {
+		slowServer := makeDelayServer(20 * time.Millisecond)
+		fastServer := makeDelayServer(0 * time.Millisecond)
 
-	defer func() {
-		slowServer.Close()
-		fastServer.Close()
+		defer func() {
+			slowServer.Close()
+			fastServer.Close()
+		}()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		serverA := makeDelayServer(11 * time.Second)
+		serverB := makeDelayServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+
+	go func() {
+		http.Get(url)
+		close(ch)
 	}()
-
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
-
-	want := fastURL
-	got := Racer(slowURL, fastURL)
-
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
-
-	slowServer.Close()
-	fastServer.Close()
+	return ch
 }
